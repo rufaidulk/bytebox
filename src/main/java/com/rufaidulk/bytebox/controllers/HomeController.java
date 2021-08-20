0482f13e26ae7772de0237a8da5f31b6b40753f0
rufaidulk <rufaidulk999@gmail.com>
2021-08-20 10:08:18 +0530

    home api

--- src/main/java/com/rufaidulk/bytebox/controllers/HomeController.java
+++ src/main/java/com/rufaidulk/bytebox/controllers/HomeController.java
@@ -1,5 +1,8 @@
 package com.rufaidulk.bytebox.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +14,10 @@ public class HomeController
     @GetMapping("/home")
     public ResponseEntity<?> home()
     {
-        return new ResponseEntity<>("Welcome to bytebox", HttpStatus.OK);
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "SUCCESS");
+        response.put("message", "Welcome to bytebox!");
+        
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
