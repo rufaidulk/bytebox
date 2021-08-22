25ea62612c9925326bbfb9d7c0578f0bbfee2976
rufaidulk <rufaidulk999@gmail.com>
2021-08-22 10:33:36 +0530

    cors config

--- src/main/java/com/rufaidulk/bytebox/config/WebSecurityConfig.java
+++ src/main/java/com/rufaidulk/bytebox/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.rufaidulk.bytebox.config;
 
+import java.util.Arrays;
+
 import com.rufaidulk.bytebox.filters.JwtAuthFilter;
 import com.rufaidulk.bytebox.services.AppUserDetailsService;
 
@@ -13,6 +15,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -40,6 +47,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
     @Override
     protected void configure(HttpSecurity http) throws Exception 
     {
+        final String corsOrigin="http://localhost:4000";
+        http.addFilterBefore(new CorsFilter(corsConfigurationSource(corsOrigin)), 
+            AbstractPreAuthenticatedProcessingFilter.class);
+        
         http.csrf().disable()
             .authorizeRequests().antMatchers("/registers", "/login").permitAll()
             .anyRequest().authenticated()
@@ -47,5 +58,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
 
         http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
     }
+        
+    private CorsConfigurationSource corsConfigurationSource(String corsOrigin) 
+    {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(corsOrigin));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","HEAD","OPTIONS","PUT","PATCH","DELETE"));
+        configuration.setMaxAge(10L);
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Accept","Access-Control-Request-Method",
+            "Access-Control-Request-Headers","Accept-Language","Authorization","Content-Type","Request-Name",
+            "Request-Surname","Origin","X-Request-AppVersion","X-Request-OsVersion", "X-Request-Device", 
+            "X-Requested-With"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        
+        return source;
+    }
+
     
 }
